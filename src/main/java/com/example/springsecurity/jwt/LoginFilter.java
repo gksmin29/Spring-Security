3ca2f77ed91d169178490bb29408aeef7f2f071e
한민 <rlemdhs2747@naver.com>
2026-03-27 16:57:10 +0900

    import문 제거

--- src/main/java/com/example/springsecurity/jwt/LoginFilter.java
+++ src/main/java/com/example/springsecurity/jwt/LoginFilter.java
@@ -9,8 +9,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import java.util.Optional;
-
 public class LoginFilter extends UsernamePasswordAuthenticationFilter {
 
     private final AuthenticationManager authenticationManager;
