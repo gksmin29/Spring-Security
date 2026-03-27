358a435729ceb266eec8d9e8ec1ee538b0f18d5d
한민 <rlemdhs2747@naver.com>
2026-03-28 00:43:21 +0900

    JWT 발급 및 검증 클래스

---
+++ src/main/java/com/example/springsecurity/jwt/JWTUtil.java
@@ -0,0 +1,49 @@
+package com.example.springsecurity.jwt;
+
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Component
+public class JWTUtil {
+
+    private SecretKey secretKey;
+
+    // .properties에 존재하는 key를 secret 변수로 받아옴
+    public JWTUtil(@Value("${spring.jwt.secret}")String secret) {
+        // key를 바탕으로 secretKey 객체 키를 만든다.
+        this.secretKey = new SecretKeySpec(
+                secret.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());
+    }
+
+    public String getUsername(String token) {
+        return Jwts.parser().verifyWith(secretKey).build().
+                parseSignedClaims(token).getPayload().get("username", String.class);
+    }
+
+    public String getRole(String token) {
+        return Jwts.parser().verifyWith(secretKey).build().
+                parseSignedClaims(token).getPayload().get("role", String.class);
+    }
+
+    public Boolean isExpired(String token) {
+        return Jwts.parser().verifyWith(secretKey).build().
+                parseEncryptedClaims(token).getPayload().getExpiration().before(new Date());
+    }
+
+    // 로그인이 성공했을 때, successfulHandler를 통해 token을 생성해서 응답해주는 메서드
+    public String createJwt(String username, String role, Long expiredMs) {
+        return Jwts.builder()
+                .claim("username", username)
+                .claim("role", role)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + expiredMs))
+                .signWith(secretKey)
+                .compact();
+    }
+}
