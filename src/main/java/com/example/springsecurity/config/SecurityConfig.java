36df8e1a1991c4c419a527a450acfc850fc44bfd
한민 <rlemdhs2747@naver.com>
2026-03-28 19:15:12 +0900

    CORS 설정

--- src/main/java/com/example/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/example/springsecurity/config/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.example.springsecurity.config;
 import com.example.springsecurity.jwt.JWTFilter;
 import com.example.springsecurity.jwt.JWTUtil;
 import com.example.springsecurity.jwt.LoginFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import org.jspecify.annotations.Nullable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -13,6 +15,10 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity  // 이 클래스는 security를 위한 config임을 명시
@@ -42,6 +48,33 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        // cors 관련 설정
+        http
+                .cors((cors) -> cors
+                        .configurationSource(new CorsConfigurationSource() {
+                            @Override
+                            public @Nullable CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+
+                                CorsConfiguration configuration = new CorsConfiguration();
+                                // 허용할 앞 단의 port
+                                configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+                                // 모든 메서드 허용
+                                configuration.setAllowedMethods(Collections.singletonList("*"));
+                                // 앞 단에서 credential을 true로 설정했다면 여기도 true로 설정해야 함
+                                configuration.setAllowCredentials(true);
+                                // 모든 헤더 허용
+                                configuration.setAllowedHeaders(Collections.singletonList("*"));
+                                // 허용 상태를 가지고 있을 시간
+                                configuration.setMaxAge(3600L);
+
+                                // 클라이언트 단으로 헤더를 보내줄 때 Authorization을 사용할 것이기 때문에
+                                configuration.setExposedHeaders(Collections.singletonList("Authorization"));
+
+                                return configuration;
+                            }
+                        }));
+
         // csrf disable
         // 세션 방식에서는 세션이 고정되기 때문에 csrf 방어를 해줘야 함
         // jwt 방식은 stateless이기 때문에 disable
